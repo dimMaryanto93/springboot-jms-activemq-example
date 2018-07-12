e8d97d4f85db4355b1439575cada13eca19bb492
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2018-07-12 11:56:06 +0700

    init project

---
+++ src/main/java/com/maryanto/dimas/example/SpringbootMessageingExampleApplication.java
@@ -0,0 +1,12 @@
+package com.maryanto.dimas.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMessageingExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMessageingExampleApplication.class, args);
+	}
+}
